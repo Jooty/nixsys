59647879245132d508b3a6c8bb7ce87b9f956d28
Jay <compundbread@gmail.com>
2023-07-19 00:46:40 +0000

    change kitty opacity

--- home/modules/kitty.nix
+++ home/modules/kitty.nix
@@ -5,7 +5,7 @@ _: {
       scrollback-lines = 10000;
       font_family = "FiraCode";
       font_size = "10.0";
-      background_opacity = "0.95";
+      background_opacity = "0.93";
     };
   };
 }
