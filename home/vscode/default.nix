659033ea5eb179b036d5e16b455ba9e763211b65
Jay <compundbread@gmail.com>
2023-09-18 04:55:37 -0400

    do some vscode work

--- home/vscode/default.nix
+++ home/vscode/default.nix
@@ -4,7 +4,7 @@
     enableUpdateCheck = false;
     enableExtensionUpdateCheck = false;
     userSettings = {
-      "editor.fontSize" = 11;
+      "editor.fontSize" = 14;
       "editor.fontLigatures" = true;
       "editor.fontFamily" = "'FiraCode Nerd Font Mono', 'Droid Sans Mono', 'monospace'";
       "files.autoSave" = "afterDelay";
@@ -12,15 +12,32 @@
       "explorer.confirmDragAndDrop" = false;
       "git.confirmSync" = false;
       "git.autofetch" = true;
-      "window.zoomLevel" = 1;
+      "window.zoomLevel" = -0.1;
       "workbench.startupEditor" = "none";
-      "workbench.colorTheme" = "Solarized Dark";
+      "workbench.colorTheme" = "Ayu Next";
       "workbench.iconTheme" = "eq-material-theme-icons-darker";
+      "background.customImages" = [
+        "https://w.wallhaven.cc/full/31/wallhaven-3ll3zv.jpg"
+      ];
     };
     extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-      zhuangtongfa.material-theme
-      roman.ayu-next
+      njpwerner.autodocstring
+      tamasfe.even-better-toml
+      usernamehw.errorlens
+      davidanson.vscode-markdownlint
+      jnoortheen.nix-ide
+      ms-python.python
+      ms-python.vscode-pylance
+      mkhl.direnv
+      editorconfig.editorconfig
+      matklad.rust-analyzer
+      kamadorueda.alejandra
+      viktorqvarfordt.vscode-pitch-black-theme
+      skellock.just
+      gruntfuggly.todo-tree
+      github.vscode-github-actions
+      github.vscode-pull-request-github
+      mhutchie.git-graph
     ];
   };
 }
